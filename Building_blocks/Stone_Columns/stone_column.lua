449b67d2d6c3e018db5fe113e9a4bc335ff77bfa
Luka22r <129538733+Luka-on-github@users.noreply.github.com>
2024-06-01 19:05:34 +0100

    Delete Building_blocks directory

--- Building_blocks/Stone_Columns/stone_column.lua
+++
@@ -1,6 +0,0 @@
-MN = "lukasblocks:"
-minetest.register_node(MN.. "stone_column", {
-	description = "Stone Column",
-	groups = {cracky = 2},
-	tiles = {"column_top.png", "column_top.png", "column_side.png"},
-})
